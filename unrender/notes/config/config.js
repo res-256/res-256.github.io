bf0ab701b1f5742e89804c940ca3b985be968572
res-256 <h1553270051@163.com>
2022-11-01 13:44:19 +0800

    Site updated: 2022-11-01 13:44:17

--- unrender/notes/config/config.js
+++ unrender/notes/config/config.js
@@ -10,7 +10,7 @@
 export default {
     lovexhj: {
         // 后端地址
-        ServerBase: "https://gitee.com/api",
+        ServerBase: "https://giteeapi.z555.icu/",
         // 码云账号
         owner: "res-256",
         // 仓库名称
